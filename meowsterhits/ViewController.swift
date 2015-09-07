93d645b31418085a65c3b2eae31ec567f8a37fcb
danlyg <gih.daniel@gmail.com>
2015-09-06 23:56:14 -0700

    working on building the game

--- meowsterhits/ViewController.swift
+++ meowsterhits/ViewController.swift
@@ -9,17 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentStackLabel: UILabel!
+    @IBOutlet weak var moveStack: UIButton!
+    @IBOutlet weak var lowerStack: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    let stackArr = [0, 0, 0, //3 zeros
+                    1, 1, 1, 1, 1, 1, 1, //7 ones
+                    2, 2, 2, 2, 2, 2, //6 twos
+                    3, 3, 3, 3, 3, //5 threes
+                    4, 4, 4, //3 fours
+                    5, 5, 5, //3 fives
+                    6, 6, 6] //3 sixes
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let pracArr = [1,2,3]
+    
+    var shuffledStacks = []
+    var gameStarted = false
+    var currentStack = 5
+    var nextStack = 10
+    
+    
+    @IBAction func startAction(sender: AnyObject) {
+        startGame()
+//        currentStack = 8
+//        nextStack = 14
+//        currentStackLabel.text = String(currentStack)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func lowerAction(sender: AnyObject) {
+        if gameStarted == false {
+            startGame()
+        }
+        
+        currentStackLabel.text = String(currentStack)
+        
+        if currentStack != 0 {
+            currentStack--
+            currentStackLabel.text = String(currentStack)
+        } else {
+            currentStackLabel.text = "You Lose"
+        }
+        
     }
+    
 
+    @IBAction func moveAction(sender: AnyObject) {
+        if gameStarted == false {
+            startGame()
+        }
+        
+        if currentStack == 0 {
+            currentStack = nextStack
+        }
+        
+        
+        currentStackLabel.text = String(currentStack)
+    }
+    
+
+    
+
+    
+
+
+    func startGame() {
+        gameStarted = true
+        shuffledStacks = pracArr.shuffled()
+        //        println(shuffledStacks)
+        
+        currentStackLabel.text = String(pracArr[0])
+    }
 
 }
 
+extension Array {
+    func shuffled() -> [T] {
+        if count < 2 { return self }
+        var list = self
+        for i in 0..<(list.count - 1) {
+            let j = Int(arc4random_uniform(UInt32(list.count - i))) + i
+            swap(&list[i], &list[j])
+        }
+        list
+        return list
+    }
+}
